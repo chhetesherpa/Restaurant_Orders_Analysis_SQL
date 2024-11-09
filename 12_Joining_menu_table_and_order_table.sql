7b523561b58a03d8553645946712f31088a4ba0e
chhetesherpa <chhete.sherpa@gmail.com>
2024-11-09 16:34:46 +0000

    edit all into 1

--- 12_Joining_menu_table_and_order_table.sql
+++ 12_Joining_menu_table_and_order_table.sql
@@ -1,9 +1,2 @@
--- 12. Combining the menu_items and order_details table into a single table
 
-SELECT 
-    *
-FROM 
-    menu_items
-    LEFT JOIN order_details
-		ON menu_items.menu_item_id = order_details.item_id;
         
--- 2_total_menu_items.sql
+++
@@ -1,6 +0,0 @@
--- 2. Find number of menu_items that the restaurant sells. 
-
-SELECT
-	COUNT(menu_item_id) AS Total_Count
-FROM 
-    menu_items;
