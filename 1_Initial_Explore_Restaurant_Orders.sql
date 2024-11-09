81476ef6fc2b98b418367e988c297d479b1ea8dd
chhetesherpa <chhete.sherpa@gmail.com>
2024-11-09 15:56:22 +0000

    4th commit

--- 1_Initial_Explore_Restaurant_Orders.sql
+++ 1_Initial_Explore_Restaurant_Orders.sql
@@ -14,26 +14,7 @@ We also noticed that the restaurant sells different categories of menu.
 
 
 
--- 4. Least and most expensive items on the menu
--- Part 1 : Least expensive menu_item
-SELECT 
-	item_name,
-    price
-FROM menu_items
-ORDER BY price
-LIMIT 1;
 
--- FINDINGS: It looks like the least expensive menu_item is Edamame with a price point of 5.00
-
--- Part 2: Most expensive menu_item
-SELECT 
-	item_name,
-    price
-FROM menu_items
-ORDER BY price DESC
-LIMIT 1;
-
--- FINDINGS: It looks like the most expensive menu_item is Shrimp Scampi with respective price of 19.95
 
 -- 5. Count number of Italian dishes in the menu
 SELECT 
